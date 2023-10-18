0ca7ebe8c99f497b1f75445405fa65d03022fd73
Abhilash BK <bkabhilash0@gmail.com>
2023-10-19 00:44:54 +0530

    Added Bootstrap

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+
+def index(request: HttpRequest):
+    # return HttpResponse("Hello World")
+    res = range(1, 15)
+    return render(request, "todo/home.html", {"todos": res})
